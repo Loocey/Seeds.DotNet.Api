0b0a00a1d1c0ba271534224db18f270c16d35ee9
Mike Pall <TheLoocey@gmail.com>
2017-10-03 08:29:27 -0700

    seed v0.1 ready

--- Loocey.Seeds.DotNet.Api/App_Start/WebApiConfig.cs
+++ Loocey.Seeds.DotNet.Api/App_Start/WebApiConfig.cs
@@ -12,17 +12,20 @@ namespace Loocey.Seeds.DotNet.Api
         {
             // Web API configuration and services
 
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+
             // Web API routes
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
 
-            config.Formatters.Remove(config.Formatters.XmlFormatter);
- 
+
+            
         }
     }
 }
---
+++ Loocey.Seeds.DotNet.Api/Controllers/TestController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace Loocey.Seeds.DotNet.Api.Controllers
+{
+
+    /// <summary>
+    /// Test Api
+    /// </summary>
+    [RoutePrefix("test")]
+    public class TestController : ApiController
+    {
+
+        /// <summary>
+        /// Returns List of strings
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// Returns the input
+        /// </summary>
+        /// <returns></returns>
+        [Route("input/{id}")]
+        [HttpGet]
+        [ResponseType(typeof(string))]
+        public string input(string id)
+        {
+            return "You entered: "+id;
+        }
+
+        /// <summary>
+        /// Returns simple string value
+        /// </summary>
+        /// <returns></returns>
+        [Route("getValue")]
+        [HttpGet]
+        public string getValue()
+        {
+            return "Simple Value Returned";
+        }
+
+
+        /// <summary>
+        /// Adds 2 numbers together
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("addNumbers")]
+        [ResponseType(typeof(int))]
+        public int addNumbers(int a, int b)
+        {
+            return a+b;
+        }
+
+
+
+    }
+}
